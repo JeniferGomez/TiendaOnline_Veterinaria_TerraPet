dae113d47f7c3841587ba860e25f1bfa96ceed71
Jenifer Andrea Gómez <jenifergomez017@gmail.com>
2023-03-07 07:59:47 -0500

    validación de correo electrónico

--- Config/Config.php
+++ Config/Config.php
@@ -9,7 +9,7 @@ const TITLE = "TERRA PET";
 const MONEDA = "COP";
 const CLIENT_ID = "Admq8yVcz_i47NxK0TsVxz96ErN0p7E3DD8s-64is8al2zF942YBLbPQAALAL7-DF9Lt_e9ZvNikfbxp";
 const USER_SMTP = "clinicaterrapetsog@gmail.com";
-const PASS_SMTP = "phbmrrklomybdalu";
+const PASS_SMTP = "ogcyrjogitcojkzc";
 const PUERTO_SMTP = 465;
 const HOST_SMTP = "smtp.gmail.com";
 ?>
