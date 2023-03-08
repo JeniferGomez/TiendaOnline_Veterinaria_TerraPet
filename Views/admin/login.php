d21dea18235a1fa796b177e00036ce415fb2305c
Jenifer Andrea Gómez <jenifergomez017@gmail.com>
2023-03-07 20:48:04 -0500

    Parte administrador

---
+++ Views/admin/login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title><?php echo $data['title']; ?></title>
+        <link href="<?php echo BASE_URL; ?>assets/css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+                                    <div class="card-body">
+                                        <form id="formulario">
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="email" name="email" type="email" value="clinicaterrapetsog@gmail.com"  placeholder="name@example.com" required>
+                                                <label for="email">Correo electronico</label>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="clave" name="clave" type="password" value="admin" placeholder="Contraseña" required>
+                                                <label for="clave">Contraseña</label>
+                                            </div>
+                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                                <a class="small" href="password.html">Has olvidado tu contraseña?</a>
+                                                <a class="btn btn-primary" type="submit" href="index.html">Login</a>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="card-footer text-center py-3">
+                                        <div class="small"><a href="register.html">¿Necesitas una cuenta? Registrate!</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Clinica TerraPet 2023</div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="<?php echo BASE_URL; ?>assets/js/scripts.js"></script>
+        <script>
+            const base_url = '<?php echo BASE_URL; ?>';
+        </script>
+        <script src="<?php echo BASE_URL; ?>assets/js/modulos/login.js"></script>
+    </body>
+</html>
