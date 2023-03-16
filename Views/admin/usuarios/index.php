ed0eefa7712825bf8cf3dbad82de6c421f8e386f
Jenifer Andrea Gómez <jenifergomez017@gmail.com>
2023-03-15 23:40:41 -0500

    Tabla usuarios

--- Views/admin/usuarios/index.php
+++ Views/admin/usuarios/index.php
@@ -1,22 +1,31 @@
 <?php include_once 'Views/template/header-admin.php'; ?>
-<div class="card">
-    <div class="card-body">
-        <div class="table-responsive">
-            <table class="table table-bordered table-striped table-hover" style="width: 100">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Nombres</th>
-                        <th>Correo</th>
-                        <th>Foto</th>
-                    </tr>
-                </thead>
-                <tbody>
-                </tbody>
-            </table>
+<div id="layoutSidenav_content">
+    <div class="container-fluid px-4">
+        <div class="card">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped table-hover" style="width: 100">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Nombres</th>
+                                <th>Apellidos</th>
+                                <th>Correo</th>
+                                <th>Foto</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+
+<script type="text/javascript" src="<?php BASE_URL . 'assets/DataTables/datatables.min.js'; ?>"></script>
+<script src="<?php BASE_URL . 'assets/js/clientes.js'; ?>"></script>
 <?php include_once 'Views/template/footer-admin.php'; ?>
 </body>
+
 </html>
--- Views/template/footer-admin.php
+++ Views/template/footer-admin.php
@@ -1,5 +1,5 @@
-</div>
-</main>
+
+
 <footer class="py-4 bg-light mt-auto">
     <div class="container-fluid px-4">
         <div class="d-flex align-items-center justify-content-between small">
